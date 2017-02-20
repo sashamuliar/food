f2128aad89855cb1787fc3b2f31b2ce1d3e69d7b
Kevin Volker <kevinvolker@MacBook-Pro-Kevin.local>
2017-02-20 20:28:45 +0200

    add_company & add_alergen page working

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var mongoClient = require('mongodb').MongoClient;
 var app = express();
 //app configurations
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 //routs
 app.use(require('./routs'));
 //acces to style things
@@ -14,6 +19,15 @@ app.use(require('./routs'));
 app.use(express.static(path.join(__dirname, 'mystyle')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.listen(3000, function(){
-  console.log('server is dancing with port 3000');
-});
+
+
+
+mongoClient.connect('mongodb://localhost:27017/food', function(err, database){
+  if (err) {
+    return console.log(err)
+  }
+  db = database;
+  app.listen(3000, function(){
+    console.log('server is dancing with port 3000');
+  });
+})
--- routs.js
+++ routs.js
@@ -6,4 +6,95 @@ router.get('/', function(req, res){
   res.render('index');
 });
 
+router.get('/add_alergen', function(req, res){
+  db.collection('alergens').find().toArray(function(err, docs) {
+      if (err) { console.log(err) };
+      var allAlergens = docs.map(function(doc){
+          return doc.name;
+        });
+      res.render('add_alergen', {
+        alergens: allAlergens
+      });
+    });
+});
+
+router.get('/add_company', function(req, res){
+  db.collection('alergens').find().toArray(function(err, docs) {
+      if (err) { console.log(err) };
+      var allAlergens = docs.map(function(doc){
+          return doc.name;
+        });
+      db.collection('companies').find().toArray(function(err, docs) {
+        if (err) { console.log(err) };
+        var allCompanies = docs.map(function(doc){
+            return doc.name;
+          });
+        res.render('add_company', {
+          alergens: allAlergens,
+          companies: allCompanies
+        });
+      })
+    });
+});
+
+//adding new alergens
+router.post('/add_alergen', function(req, res){
+  var alergen = { name: req.body.new_alergen };
+  db.collection('alergens').find().toArray(function(err, docs) {
+      if (err) { console.log(err) };
+      var allAlergens = docs.map(function(doc){
+          return doc.name;
+      });
+      var rule = false;
+      allAlergens.forEach(function(single){
+        if (single === alergen.name) {
+          return rule = true;
+        };
+      });
+      if (rule) {
+        res.redirect('/add_alergen');
+      } else {
+        db.collection('alergens').insert(alergen, function(err, result){
+          if (err) console.log(err);
+          res.redirect('/add_alergen');
+        });
+      }
+  });
+});
+
+//deleting alergens
+
+router.post('/delete_alergen', function(req, res){
+  console.log(req.body.delete_alergen);
+  db.collection('alergens').deleteOne(
+    {name: req.body.delete_alergen},
+    function(err, result) {
+      if (err){
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      res.send('deleted ' + req.body.delete_alergen);
+    }
+  );
+})
+
+router.post('/add_company', function(req, res){
+  var newCompany = {
+    name: req.body.new_company,
+    kitchen: req.body.kitchen,
+    about: req.body.about_company
+  };
+  newCompany.dishes = [];
+  newCompany.dishes.push({
+    dish:req.body.dish_name,
+    alergens:req.body.alergens
+  });
+  db.collection('companies').insert(newCompany, function(err, result){
+    if (err) console.log(err);
+    res.redirect('/add_company');
+  });
+  console.log('Company ' + newCompany.name + ' added!');
+});
+
+
 module.exports = router;
