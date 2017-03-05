1d462b594b71ae09ec0b2ceedf856b1e71c5aebd
Kevin Volker <kevinvolker@MacBook-Pro-Kevin.local>
2017-03-05 17:02:34 +0200

    lgoin\register

--- app.js
+++ app.js
@@ -3,32 +3,67 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoClient = require('mongodb').MongoClient;
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
 
 var app = express();
 //app configurations
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+//middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(session({secret: 'boo', saveUninitialized: false, resave: false}));
 
+//passport init
+app.use(passport.initialize());
+app.use(passport.session());
+//flash
+app.use(flash());
+//global validationErrors
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 //routs
-app.use(require('./routs'));
+app.use('/', require('./routs'));
 
 //acces to style things
 app.use(express.static(path.join(__dirname, 'mystyle')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 //PORT
 var PORT = process.env.PORT || 3000;
+//db uri
+var uri = 'mongodb://localhost/food';
 
 
-
-mongoClient.connect('mongodb://localhost:27017/food', function(err, database){
-  if (err) {
-    return console.log(err)
+//conection to db
+mongoose.connect(uri, function(error){
+  if (error){
+    return console.log('failed to connect to db');
+  } else {
+    console.log('connection is good');
+    app.listen(PORT, function(){
+      console.log('server is dancing with port ' + PORT);
+    })
   }
-  db = database;
-  app.listen(PORT, function(){
-    console.log('server is dancing with port ' + PORT);
-  });
-})
+});
+// mongoClient.connect('mongodb://localhost:27017/food', function(err, database){
+//   if (err) {
+//     return console.log(err)
+//   }
+//   db = database;
+//   app.listen(PORT, function(){
+//     console.log('server is dancing with port ' + PORT);
+//   });
+// })
--- routs.js
+++ routs.js
@@ -1,15 +1,11 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var prod = [{
-  id: 1,
-  name:'laptop'
-},
-{
-  id:2,
-  name:'micro'
-}];
+
 router.get('/', function(req, res){
   res.render('index');
 });
@@ -109,7 +105,7 @@ router.post('/delete_alergen', function(req, res){
     }
   );
 })
-
+// adding new companies
 router.post('/add_company', function(req, res){
   var newCompany = {
     name: req.body.new_company,
@@ -127,6 +123,113 @@ router.post('/add_company', function(req, res){
   });
   console.log('Company ' + newCompany.name + ' added!');
 });
+
+router.get('/register', function(req, res){
+  res.render('register',{errors: undefined});
+
+});
+
+router.post('/register', function(req, res){
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is required').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors){
+    res.render('register', {errors:errors});
+    console.log(errors);
+  } else {
+    var newUser = new User({
+      name:req.body.name,
+      username:req.body.username,
+      email:req.body.email,
+      password:req.body.password
+    });
+    newUser.save(function(){
+      console.log('user saved');
+    })
+    console.log(newUser);
+    req.flash('success_msg', 'You are registered and now can login');
+    res.redirect('login');
+  };
+
+
+});
+
+router.get('/login', function(req, res){
+  res.render('login', {msg:undefined});
+});
+///////////////////////
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        console.log('no user');
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.comparePasswords(password, user.password)) {
+        console.log('wrong pass');
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      console.log('passed');
+      return done(null, user);
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+router.post('/login',
+  passport.authenticate('local',
+  {
+    successRedirect:'/',
+    failureRedirect:'/login',
+    failureFlash: true,
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+router.get('/logout', function(req, res){
+  req.logout();
+  
+  req.flash('success_msg', 'You are logged out');
+
+  res.redirect('/login');
+});
+
+/////////////////////////
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  name: String,
+  username: {
+    type: String,
+    index: true
+  },
+  email: String,
+  password: String
+});
+
+UserSchema.methods.comparePasswords = function(enteredPassword, storedPassword){
+  console.log('User ' + storedPassword);
+  return enteredPassword === storedPassword;
+};
+
+var User = mongoose.model('User', UserSchema);
+
 
 
 module.exports = router;
